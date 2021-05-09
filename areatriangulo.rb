66de33f061d138d366893349a03c5ee43b5b1b0f
jhondiery <>
2021-05-09 17:31:44 +0000

    ejemplos

---
+++ areatriangulo.rb
@@ -0,0 +1,11 @@
+#definir variables y otros
+puts "hola mundo"
+#datos de entrada por dispocitivos de entrada
+print"ingrese base:" 
+b=gets.to_i
+print"ingrese altura:"
+h=gets.to_i
+#proceso
+area=(b*h)/2
+#datos de salida
+puts "el area del triangulo es: #{area}"
